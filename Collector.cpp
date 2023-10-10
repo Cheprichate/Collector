6bf8734798b7a2f0e5a9ce74952de0022de5b7d5
Cheprichate <petrovich15071998@mail.ru>
2023-10-10 11:15:04 +0400

    Добавьте файлы проекта.

---
+++ Collector.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+int score;
+int debt;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    std::cout << "Какой у вас долг \n";
+    std::cin >> debt;
+
+    while (debt > 0) {
+        std::cout << "Плоти свой долг в " << debt << " \n";
+        std::cin >> score;
+        debt -= score;
+    }
+    if (score > debt) {
+        std::cout << "Остаток" << debt + score;
+    }
+    else {
+        std::cout << "Долг уплачен ";
+    }
+}
